828ae783fc23eab3de6df842a93ad395f72e0d4a
Paxton Hare <paxton@greenllama.com>
2020-08-30 13:31:02 -0400

    fixes #13

--- demo-magic.sh
+++ demo-magic.sh
@@ -124,9 +124,7 @@ function p() {
 function pe() {
   # print the command
   p "$@"
-
-  # execute the command
-  eval "$@"
+  run_cmd "$@"
 }
 
 ##
@@ -154,7 +152,19 @@ function cmd() {
   x=$(PS1="$DEMO_PROMPT" "$BASH" --norc -i </dev/null 2>&1 | sed -n '${s/^\(.*\)exit$/\1/p;}')
   printf "$x\033[0m"
   read command
-  eval "${command}"
+  run_cmd "${command}"
+}
+
+function run_cmd() {
+  function handle_cancel() {
+    printf ""
+  }
+
+  trap handle_cancel SIGINT
+  stty -echoctl
+  eval "$@"
+  stty echoctl
+  trap - SIGINT
 }
 
 
