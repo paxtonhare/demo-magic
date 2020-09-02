10abf5390b6d5461155dc58012081490598f7bad
Bright Zheng <bright.zheng@outlook.com>
2020-09-02 18:58:57 +0800

    add new features to the sample script (#22)

--- samples/demo-template.sh
+++ samples/demo-template.sh
@@ -34,11 +34,35 @@ if [ ! -d "stuff" ]; then
   pe "mkdir stuff"
 fi
 
+# print and execute: cd stuff
 pe "cd stuff"
 
-pe "ls"
+# ctl + c support: ctl + c to stop long-running process and continue demo
+pe "ping www.google.com"
 
+# print and execute: echo 'hello world' > file.txt
+pe "echo 'hello world' > file.txt"
+
+# wait max 3 seconds until user presses
+PROMPT_TIMEOUT=3
+wait
+
+# print and execute immediately: ls -l
+pei "ls -l"
+# print and execute immediately: cat file.txt
+pei "cat file.txt"
+
+# and reset it to manual mode to wait until user presses enter
+PROMPT_TIMEOUT=0
+
+# print only
 p "cat \"something you want to pretend to run\""
+
+# run command behind
+cd .. && rm -rf stuff
+
+# enters interactive mode and allows newly typed command to be executed
+cmd
 
 # show a prompt so as not to reveal our true nature after
 # the demo has concluded
